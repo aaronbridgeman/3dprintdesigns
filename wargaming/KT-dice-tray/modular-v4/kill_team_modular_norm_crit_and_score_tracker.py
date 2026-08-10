13bd675fb76a9c29e81ac5a5430c12087923ea12
aaronbridgeman <abridgeman85@gmail.com>
2026-08-10 15:26:43 +0800

    KT modular-v4: adjust text sizes for visibility and fit

--- wargaming/KT-dice-tray/modular-v4/kill_team_modular_norm_crit_and_score_tracker.py
+++ wargaming/KT-dice-tray/modular-v4/kill_team_modular_norm_crit_and_score_tracker.py
@@ -70,10 +70,10 @@ label_cut_h = 0.5
 storage_well_cut_h = 7.0
 
 # Programmatic text engraving
-norm_crit_text_size = 2.6
-score_center_text_size = 3.2
-side_label_text_size = 3.0
-text_cut_h = 0.4
+norm_crit_text_size = 3.5
+score_center_text_size = 3.5
+side_label_text_size = 4.5
+text_cut_h = 0.6
 
 FONT_CANDIDATES = [
     "C:/Windows/Fonts/arialbd.ttf",
